06099de782e6d928904a1701afa473ffe01759b9
rpritr <robi@pritrznik.si>
2026-07-15 12:51:55 +0200

    update server

--- src/Components/Task/TaskList/TaskList.jsx
+++ src/Components/Task/TaskList/TaskList.jsx
@@ -3,17 +3,39 @@ import TaskItem from "../TaskItem/TaskItem.jsx";
 import "./TaskList.scss";
 const TaskList = () => {
     const [tasks, setTasks] = useState([]);
-    const url = "http://localhost:3000/tasks";
-
+    const url = "https://api.jsonsilo.com/b56253e9-9bb1-40f8-aaac-bc5797bb9f4c";
+    const [error, setError] = useState(null);
     useEffect(() => {
 
         const fetchTasks = async () => {
-            const r = await fetch(url, { method: "GET" });
-            const tasks = await r.json();
-            setTasks(tasks);
-        }
+
+            try {
+                const response = await fetch(url, {
+                    method: "GET",
+                    headers: {
+                        "X-SILO-KEY": import.meta.env.VITE_JSON_KEY,
+                    },
+
+                });
+                const data = await response.json();
+                console.log(data);
+                setTasks(data.tasks);
+
+            } catch (error) {
+                console.error(error);
+                setError(error.message);
+            }
+        };
+
         fetchTasks();
-    })
+
+    }, [url]);
+
+    if (error) {
+
+        return <p>{error}</p>;
+
+    }
     return (
         <div className="task-list">
             {tasks.map((task, index) => (
